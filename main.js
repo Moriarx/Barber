1f93bb223edc40ff1eb886a05b06a2be8c73f1c7
Jan <jdsolutions.work@gmail.com>
2020-08-11 16:19:08 +0200

    Added calendar and contact section.

--- main.js
+++ main.js
@@ -1,12 +1,99 @@
 //Adding a black bar over menu while scrolling down.
-const header = document.querySelector('.main-header');
-window.addEventListener('scroll', () => {
-    const scrollPosition = window.scrollY;
-    if(scrollPosition > 10){
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
+const header = document.querySelector(".main-header");
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+  if (scrollPosition > 10) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
 });
 
+let map;
 
+function initMap() {
+  map = new google.maps.Map(document.querySelector(".contact-map"), {
+    center: { lat: 40.7128, lng: -74.006 },
+    zoom: 18,
+  });
+}
+
+//CALENDAR
+
+const date = new Date();
+
+const renderCalendar = () => {
+  date.setDate(1);
+
+  const monthDays = document.querySelector(".days");
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+  const firstDayIndex = date.getDay();
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  document.querySelector(".date h2").innerHTML = months[date.getMonth()];
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class="today">${i}</div>`;
+    }
+    days += `<div>${i}</div>`;
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class ="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
+};
+
+renderCalendar();
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
